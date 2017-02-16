e9b5a9375f614dc5db633e3a31b7db43e62f70b1
Vivienne Ho <vkh24@drexel.edu>
2017-02-16 16:17:48 -0500

    Homepage UI

---
+++ V/Homepage2UI/app/src/main/java/com/loginui/drexelclubapp/homepage2ui/HomepageUI.java
@@ -0,0 +1,13 @@
+package com.loginui.drexelclubapp.homepage2ui;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class HomepageUI extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_homepage_ui);
+    }
+}
